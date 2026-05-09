b0a9c7116c14ef22cf732293f6fd1b4f23011a11
Melvin-Austin <ausmel19540@nbss.nbed.nb.ca>
2026-05-09 15:02:47 -0300

    Release v1.0.5

--- obj/Release/netstandard2.1/SOSInsurance.AssemblyInfo.cs
+++ obj/Release/netstandard2.1/SOSInsurance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOSInsurance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c11622914b1d5bd67218eaf680427b6475fb19b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+983d433da7c5a53810f0c1b90131b51dde0ccca2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SOSInsurance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SOSInsurance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
